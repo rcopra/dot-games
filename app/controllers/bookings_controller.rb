b63fdd35bf25dbe2f8f7493434a008a966897866
Richard Copra <rickcopra@gmail.com>
2023-08-22 12:34:25 +0200

    Added booking button to games show page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,30 +1,26 @@
 class BookingsController < ApplicationController
 
-  def index
-    @bookings = Booking.all
-  end
+# simple form is necessary for new page -
+# form should be directly on the show page
 
-  def show
+def create
+  if user_signed_in?
+  raise
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    if @booking.save
+      redirect_to @booking, notice: "booking was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+end
+
+  private
+
+
+  def find_booking
     @booking = Booking.find(params[:id])
   end
 
-  def new
-
-  end
-
-  def create
-
-  end
-
-  def edit
-
-  end
-
-  def update
-
-  end
-
-  def destroy
-
-  end
 end
