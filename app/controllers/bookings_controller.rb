2188dc698c0ecee92d7ed488b57608b8a2bfa127
Richard Copra <rickcopra@gmail.com>
2023-08-25 12:07:52 +0200

    Fixed some permissions with users being able to book their own games, and some login items are hidden now

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,18 +2,20 @@ class BookingsController < ApplicationController
   before_action :authenticate_user!
 
 def create
-  @game = Game.find(params[:game_id])
-  @booking = Booking.new(booking_params)
-  @booking.game = @game
-  @booking.user = current_user
-  if @booking.save
-    redirect_to @game, notice: "Booking was successfully created."
-  else
-    render :new, status: :unprocessable_entity
+  if current_user != @game.user
+    @game = Game.find(params[:game_id])
+    @booking = Booking.new(booking_params)
+    @booking.game = @game
+    @booking.user = current_user
+    if @booking.save
+      redirect_to @game, notice: "Booking was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
 
-private
+  private
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
