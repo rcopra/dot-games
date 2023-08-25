78fb69b776867209acb15b01ae11706f5511fce3
Oleksandra Tovshchyk <alexestov@gmail.com>
2023-08-25 14:39:50 +0200

    Fixed the booking button

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,20 +2,18 @@ class BookingsController < ApplicationController
   before_action :authenticate_user!
 
 def create
-  if current_user != @game.user
-    @game = Game.find(params[:game_id])
-    @booking = Booking.new(booking_params)
-    @booking.game = @game
-    @booking.user = current_user
-    if @booking.save
-      redirect_to @game, notice: "Booking was successfully created."
-    else
-      render :new, status: :unprocessable_entity
-    end
+  @game = Game.find(params[:game_id])
+  @booking = Booking.new(booking_params)
+  @booking.game = @game
+  @booking.user = current_user
+  if @booking.save
+    redirect_to @game, notice: "Booking was successfully created."
+  else
+    render :new, status: :unprocessable_entity
   end
 end
 
-  private
+private
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
